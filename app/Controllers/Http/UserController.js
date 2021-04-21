d9a36f28a0d26cce6c19110c58c4a3da3d96473e
Cayo Syllos <cayodesyllos@gmail.com>
2021-04-21 17:20:16 -0300

    update user status

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,5 +1,6 @@
 "use strict";
 const User = use("App/Models/User");
+const Event = use("Event");
 
 class UserController {
   async store({ request, response, auth }) {
@@ -10,6 +11,25 @@ class UserController {
       const token = await auth.attempt(data.email, data.password);
 
       return token;
+    } catch (error) {
+      return response.status(400).send({
+        error: { message: error.message },
+      });
+    }
+  }
+
+  async update({ request, response, auth }) {
+    const { infected } = request.only(["infected"]);
+
+    try {
+      auth.user.infected = infected;
+      await auth.user.save();
+
+      if (infected) {
+        Event.fire("user::updateStatus", auth.user.id);
+      }
+
+      return;
     } catch (error) {
       return response.status(400).send({
         error: { message: error.message },
